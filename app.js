28b5eab1935eeec46908c1ed9493397147ef7bc0
Tim Emiola <adetokunbo@users.noreply.github.com>
2021-03-30 12:08:03 +0900

    Add an endpoint for dumping the page html

--- app.js
+++ app.js
@@ -14,7 +14,10 @@ var parseUrl = function(url) {
 };
 
 app.get('/', function(req, res) {
-    var urlToScreenshot = parseUrl(req.query.url);
+  var urlToScreenshot = parseUrl(req.query.url);
+  const notFull = !!req.query.notFull;
+  const fullPage = !notFull;
+
 
     if (validUrl.isWebUri(urlToScreenshot)) {
         console.log('Screenshotting: ' + urlToScreenshot);
@@ -25,7 +28,7 @@ app.get('/', function(req, res) {
 
             const page = await browser.newPage();
             await page.goto(urlToScreenshot);
-            await page.screenshot({fullPage: true}).then(function(buffer) {
+            await page.screenshot({fullPage: fullPage}).then(function(buffer) {
                 res.setHeader('Content-Disposition', 'attachment;filename="' + urlToScreenshot + '.png"');
                 res.setHeader('Content-Type', 'image/png');
                 res.send(buffer);
@@ -36,6 +39,35 @@ app.get('/', function(req, res) {
     } else {
         res.send('Invalid url: ' + urlToScreenshot);
     }
+
+});
+
+
+app.get('/body', function(req, res) {
+  var urlToScrape = parseUrl(req.query.url);
+  const notFull = !!req.query.notFull;
+  const fullPage = !notFull;
+
+
+  if (validUrl.isWebUri(urlToScrape)) {
+    console.log('Screenshotting: ' + urlToScrape);
+    (async() => {
+      const browser = await puppeteer.launch({
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+      });
+
+      const page = await browser.newPage();
+      await page.goto(urlToScrape);
+      await page.content().then(function(buffer) {
+        res.setHeader('Content-Type', 'text/html');
+        res.send(buffer);
+      });
+
+      await browser.close();
+    })();
+  } else {
+    res.send('Invalid url: ' + urlToScrape);
+  }
 
 });
 
